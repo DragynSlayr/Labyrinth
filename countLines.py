b8ce594cb1444e0d71079ada82d11aeada4f3c60
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2019-06-27 10:33:13 -0600

    Made output clearer, shows distribution now

--- countLines.py
+++ countLines.py
@@ -2,14 +2,22 @@ import os
 
 lines = 0
 
+counts = []
+
 for path, dir, files in os.walk('.'):
     if not path.startswith('.\\.git'):
         for file in files:
             if file.endswith('.moon'):
                 with open(path + '\\' + file, 'r') as f:
-                    print("Counting %s" % (path + '\\' + file))
+                    num_lines = 0
                     for line in f:
                         if len(line.strip()) > 0:
                             lines += 1
+                            num_lines += 1
+                    counts.append((num_lines, path + '\\' + file))
 
-print("%d lines counted" % lines)
+s = "%d lines in total" % lines
+print(s)
+print(len(s) * '~')
+for (num, f) in sorted(counts, key = lambda x: x[0], reverse=True):
+    print("%d\t%.2f%%\t%s" % (num, (num * 100.0) / lines, f))
